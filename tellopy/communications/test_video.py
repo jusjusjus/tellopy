f735e0f49632052a42c501e7ccc53f370e305aae
Justus Schwabedal <jschwabedal@gmail.com>
2020-12-20 19:14:05 +0100

    Refactor video test

--- tellopy/communications/test_video.py
+++ tellopy/communications/test_video.py
@@ -4,14 +4,14 @@ from .video import Video
 
 def test_video():
     video = Video("/dev/video0")
-    assert video.running is False
+    assert not video.running
     video.start()
-    assert video.running is True
+    assert video.running
     f = video.frame
     assert isinstance(f, np.ndarray) and f.shape[2] == 3, str(f)
     video.stop()
-    assert video.running is False
+    assert not video.running
     video.start()
-    assert video.running is True
+    assert video.running
     f = video.frame
     assert isinstance(f, np.ndarray) and f.shape[2] == 3, str(f)
