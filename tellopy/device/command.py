20c1d1f25c495f77089ec800fc5538c5bf5fb11f
Justus Schwabedal <jschwabedal@gmail.com>
2021-01-17 19:01:09 +0100

    Fix(__main__): manual control

---
+++ tellopy/device/command.py
@@ -0,0 +1,51 @@
+class Command:
+    level_1_commands = [
+        'streamoff',
+        'streamon',
+        'command',
+        'takeoff',
+        'land',
+    ]
+    level_2_commands = [
+        'forward',
+        'back',
+        'down',
+        'ccw',
+        'up',
+        'cw',
+    ]
+    level_3_commands = [
+        'flip',
+    ]
+    def __init__(self, command: str):
+        self.command = command
+
+    def tobytes(self):
+        return self.command.encode('utf-8')
+
+    def validate(self):
+        try:
+            self._validate()
+        except AssertionError as err:
+            raise ValueError(f"invalid command '{self.command}'")
+
+    def _validate(self):
+        split = self.command.split()
+        assert len(split) in [1, 2]
+        if len(split) == 1:
+            assert split[0] in self.level_1_commands
+        elif len(split[0]) == 2:
+            command, value = split
+            if command in self.level_2_commands:
+                assert value.isnumeric()
+            elif command in self.level_3_commands:
+                assert value == 'b'
+
+    def is_init_command(self):
+        return self.command == 'command'
+
+    @classmethod
+    def from_string(cls, string):
+        instance = cls(string)
+        instance.validate()
+        return instance
